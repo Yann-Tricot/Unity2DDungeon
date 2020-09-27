c41c598bb2c293fe228961f88cf43dbdef1ddfcf
Tricot Yann <tricot.yann@gmail.com>
2020-09-27 15:42:23 +0200

    V1.0 - First Release Dungeon Generator

--- Unity2DDungeon_Project/Assets/Scripts/DungeonGenerator/RoomSpawner.cs
+++ Unity2DDungeon_Project/Assets/Scripts/DungeonGenerator/RoomSpawner.cs
@@ -6,36 +6,61 @@ public class RoomSpawner : MonoBehaviour
 {
     public int openingDirection;
     private RoomTemplates templates;
+    private bool isFirstGeneration;
     private int rand;
     private bool spawned;
     public float waitTime = 4f;
 
     private void Start() {
         Destroy(gameObject, waitTime);
-        templates= GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
         Invoke("Spawn", 0.3f);
     }
 
     // Update is called once per frame
     void Spawn()
     {
+        isFirstGeneration = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>().firstGeneration;
         if(spawned == false){
             if(openingDirection == 1){
             // Spawning BOTTOM room
-            rand = Random.Range(0, templates.bottomRooms.Length);
-            Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].transform.rotation);
+            if(isFirstGeneration == true){
+                rand = Random.Range(0, templates.firstBottomRooms.Length);
+                Instantiate(templates.firstBottomRooms[rand], transform.position, templates.firstBottomRooms[rand].transform.rotation);
+            }else{
+                rand = Random.Range(0, templates.lastBottomRooms.Length);
+                Instantiate(templates.lastBottomRooms[rand], transform.position, templates.lastBottomRooms[rand].transform.rotation);
+            }
+            
             } else if(openingDirection ==2){
                 // Spawning TOP room
-                rand = Random.Range(0, templates.topRooms.Length);
-                Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
+                if(isFirstGeneration == true){
+                rand = Random.Range(0, templates.firstTopRooms.Length);
+                Instantiate(templates.firstTopRooms[rand], transform.position, templates.firstTopRooms[rand].transform.rotation);
+                }else{
+                    rand = Random.Range(0, templates.lastTopRooms.Length);
+                    Instantiate(templates.lastTopRooms[rand], transform.position, templates.lastTopRooms[rand].transform.rotation);
+                }
+            
             } else if(openingDirection ==3){
                 // Spawning LEFT room
-                rand = Random.Range(0, templates.leftRooms.Length);
-                Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
+                if(isFirstGeneration == true){
+                rand = Random.Range(0, templates.firstLeftRooms.Length);
+                Instantiate(templates.firstLeftRooms[rand], transform.position, templates.firstLeftRooms[rand].transform.rotation);
+                }else{
+                    rand = Random.Range(0, templates.lastLeftRooms.Length);
+                    Instantiate(templates.lastLeftRooms[rand], transform.position, templates.lastLeftRooms[rand].transform.rotation);
+                }
+
             } else if(openingDirection ==4){
                 // Spawning RIGHT room
-                rand = Random.Range(0, templates.rightRooms.Length);
-                Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
+                if(isFirstGeneration == true){
+                rand = Random.Range(0, templates.firstRightRooms.Length);
+                Instantiate(templates.firstRightRooms[rand], transform.position, templates.firstRightRooms[rand].transform.rotation);
+                }else{
+                    rand = Random.Range(0, templates.lastRightRooms.Length);
+                    Instantiate(templates.lastRightRooms[rand], transform.position, templates.lastRightRooms[rand].transform.rotation);
+                }
             }
             spawned = true;
         }
--- Unity2DDungeon_Project/Assets/Scripts/DungeonGenerator/RoomTemplates.cs
+++ Unity2DDungeon_Project/Assets/Scripts/DungeonGenerator/RoomTemplates.cs
@@ -3,28 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RoomTemplates : MonoBehaviour
-{
-    public GameObject[] bottomRooms;
-    public GameObject[] topRooms;
-    public GameObject[] leftRooms;
-    public GameObject[] rightRooms;
+{    
+    public int mapSize;
+    public bool firstGeneration;
+    public GameObject[] firstBottomRooms;
+    public GameObject[] firstTopRooms;
+    public GameObject[] firstLeftRooms;
+    public GameObject[] firstRightRooms;
+    public GameObject[] lastBottomRooms;
+    public GameObject[] lastTopRooms;
+    public GameObject[] lastLeftRooms;
+    public GameObject[] lastRightRooms;
     public GameObject closedRooms;
     public List<GameObject> rooms;
-
-    public float waitTime;
     private bool spawnedBoss;
     public GameObject boss;
+    private int roomsCount;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        firstGeneration = true;
+        roomsCount = 0;
+    }
 
     /// <summary>
     /// Update is called every frame, if the MonoBehaviour is enabled.
     /// </summary>
     void Update()
     {
-        if(waitTime <= 0 && !spawnedBoss){
+        int currentRoomsCount = rooms.Count;
+        if(rooms.Count >= mapSize) firstGeneration = false;
+        if(!spawnedBoss && firstGeneration == false && currentRoomsCount == roomsCount){
+            int i;
             Instantiate(boss, rooms[rooms.Count-1].transform.position, Quaternion.identity);
             spawnedBoss = true;
-        }else{
-            waitTime -= Time.deltaTime;
         }
+        roomsCount = rooms.Count;
     }
 }
